8f2f050117ef9333bef4c2bfd982345f4624412b
David <daviddenghaotian@163.com>
2018-06-01 19:43:18 +0800

    Renamed old transfer.sh to V1_transfer.sh

--- transfer/transfer.sh
+++ transfer/transfer.sh
@@ -1,12 +1,61 @@
 #!/bin/bash
-# This script moves all files in ~/Download folder into the current directory. It is often used after some files are downloaded.
+# This is an improved version of the previous transfer.sh, which can 
+# 1. handle space in file names correctly, 
+# 2. skip files being downloaded (files that match DownloadingRegex)
+# 3. retry automatically after a few seconds
 DownloadDIR=/home/wallet/Downloads
-files="$(ls -A $DownloadDIR)"
-isDownloading="$(find $DownloadDIR -maxdepth 1 -mindepth 1 -name "*.part")"
-if [[ -n "$isDownloading" ]]; then
-    /usr/local/bin/select_transfer.sh "$isDownloading"
-elif [[ -n "$files" ]]; then
-    mv ~/Downloads/* . && echo -e "File(s):\n$files\nmoved to $PWD"
-else
-    echo "Folder ~/Downloads is empty"
-fi
+DownloadingRegex="^.*\.part$"
+SpacedRegex="^.* .*$"
+RetryCountDown=10
+
+transfer()
+{
+	DownloadingIdx=0
+	DownloadedIdx=0
+	# Declare 2 arrays
+	declare -a downloadingFiles # Stores all 'a' and 'a.part' files as 'a'
+	declare -a downloadedFiles # Stores all files 'a' without a corresponding 'a.part'
+	for file in $DownloadDIR/*
+	do
+		if [[ $file =~ $DownloadingRegex ]]; then continue; fi # skip all files in the form 'a.part'
+		if [ -f "${file}.part" ]; then # if file is being downloaded
+			downloadingFiles[$DownloadingIdx]=${file} 
+			((DownloadingIdx++))
+		else
+			downloadedFiles[$DownloadedIdx]=${file}
+			((DownloadedIdx++))
+		fi
+	done
+
+	if [ ${#downloadedFiles[@]} != 0 ]; then
+		echo "${#downloadedFiles[@]} file(s) to be moved to $PWD:"
+		for i in "${downloadedFiles[@]}"
+		do
+			mv "$i" $PWD 
+			echo "moving ${i##*/}..."
+		done
+		echo "Done!"
+		echo ""
+	fi
+
+	if [ ${#downloadingFiles[@]} != 0 ]; then
+		echo "${#downloadingFiles[@]} file(s) being downloaded, thus not moved:"
+		for i in "${downloadingFiles[@]}"
+		do
+			echo "$i"
+		done
+
+		# count down and retry
+		for i in $(seq ${RetryCountDown} 0)
+		do
+			printf "Retry in $i... (press Ctrl-C to terminate)"
+			sleep 1
+			printf "\r										  \r"
+		done
+		echo ""
+		transfer
+	fi
+}
+
+transfer
+
