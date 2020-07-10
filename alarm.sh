3d1e8b324d8e444012c23ebc296e9289133c0349
David Deng <daviddenghaotian@163.com>
2020-07-10 14:16:07 -0400

    added interrupt trap to alarm.sh

--- alarm.sh
+++ alarm.sh
@@ -3,11 +3,21 @@
 SOUND_FILE="/home/wallet/Music/source/Electrical_Sweep-Sweeper-1760111493.wav"
 # Maximum time for the alarm to last
 MAX_SEC=60
+PID="INIT"
+
+cleanup() {
+	# kill both the loop and its children (aplay)
+	[ "$PID" != "INIT" ] && kill -9 $PID $(ps --ppid $PID --no-heading -o pid) 
+	exit 0
+}
+
+trap cleanup SIGINT
 
 while :; do aplay -q $SOUND_FILE ; done &
 PID=$!
 read -t $MAX_SEC -s -r -n 1 -p "Press any button to stop the alarm..." 
 echo
 
-# kill both the loop and its children (aplay)
-kill -9 $PID $(ps --ppid $PID --no-heading -o pid ) 
+cleanup
+
+
