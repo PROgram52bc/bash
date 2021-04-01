2facf0ca44dee0fc30533195c3ce9bad6e73563a
David Deng <daviddenghaotian@163.com>
2021-03-31 21:59:27 -0400

    scanhost accept a username

--- scanhosts.sh
+++ scanhosts.sh
@@ -8,8 +8,9 @@ fi
 echo "$HOSTCOLLECTIONS"
 
 for host in $(eval echo $HOSTCOLLECTIONS); do
+	host_name=${host#*@}
 	echo "Trying to connect to $host:22..."
-	if nc -w 1 $host 22 > /dev/null 2>&1; then # timeout 1 second
+	if nc -w 1 $host_name 22 > /dev/null 2>&1; then # timeout 1 second
 		# if successfully connected, quit and report
 		echo "================$host is open!================"
 		echo "Connecting with ssh..."
